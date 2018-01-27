d6e4bdf8188ab63a7b1de9f6c3f003c94e321916
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2018-01-27 18:24:04 -0500

    Updated fix for RGB

--- ReadingRGB.py
+++ ReadingRGB.py
@@ -1,7 +1,6 @@
 from PIL import Image
 im = Image.open("dead_parrot.jpg") #Can be many different formats.
 pix = im.load()
-print (im.size)
 rgb_im = im.convert('RGB')
-r, g, b = rgb_im.getpixel((1, 1))
+r, g, b = rgb_im.getpixel((0, 0))
 print (r,g,b)
