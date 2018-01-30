e8263bb11e7756c48aae0e250e314dd52e7365aa
Alvyn <alvyn279@gmail.com>
2018-01-29 23:32:10 -0500

    Generated csv files for AI training
    - Attempt 1 for new algorithm
    - Trying to solve issue #3

--- ReadingRGB.py
+++ ReadingRGB.py
@@ -1,28 +1,27 @@
 from PIL import Image
+import math
 
 def AveragePixelRGB(pix_list):
-    red = 0
-    green = 0
-    blue = 0
-    for val in range(0,len(pix_list),8):
-        red += pix_list[val][0]
-        green += pix_list[val][1]
-        blue += pix_list[val][2]
-    total = len(pix_list)/8
-    red /= total
-    green /= total
-    blue /= total
-    red = int(round(red, 0))
-    green = int(round(green, 0))
-    blue = int(round(blue, 0))
+    try:
+        red = int(pix_list[0][0])
+        green = int(pix_list[0][1])
+        blue = int(pix_list[0][2])
+    except:
+        return 180, 180, 180
+        
+    for val in range(0,len(pix_list),10):
+        red = get_two_average(red,pix_list[val][0])
+        green = get_two_average(green,pix_list[val][1])
+        blue = get_two_average(blue, pix_list[val][2])
+    
+    red = int(round(red))
+    green = int(round(green))
+    blue = int(round(blue))
     return red, green, blue
     
-im = Image.open("photo.png",'r') #Can be many different formats.
-pix = im.load()
-pix_val = list(im.getdata())
-rgb_im = im.convert('RGB')
-r, g, b = rgb_im.getpixel((0, 0))
-
-print (r,g,b)
-print (AveragePixelRGB(pix_val))
-
+def get_two_average(color1, color2):
+    square1 = color1*color1
+    square2 = color2*color2
+    partial_sum = square1 + square2
+    partial_sum /= 2
+    return  math.sqrt(partial_sum)
--- rgb_fetcher.py
+++ rgb_fetcher.py
@@ -1,46 +1,34 @@
-from io import BytesIO, StringIO
-from urllib.request import urlopen
-from zipfile import ZipFile
+# from io import BytesIO, StringIO
+# from urllib.request import urlopen
+# from zipfile import ZipFile
 import csv
 from PIL import Image
 import glob
 from ReadingRGB import *
+
 # from StringIO import StringIO
 
 colors = {"black", "white", "grey", "brown", "green", "blue", "yellow", "orange", "red", "purple", "pink"}
 
 base_url = "https://cvhci.anthropomatik.kit.edu/~bschauer/datasets/google-512/orig/"
 
-# def get_average_color(x,y, n, image):
-# 	r, g, b = 0, 0, 0
-# 	count = 0
-# 	for s in range(x, x+n+1):
-# 		for t in range(y, y+n+1):
-# 			pixlr, pixlg, pixlb = image[s, t]
-# 			r += pixlr
-# 			g += pixlg
-# 			b += pixlb
-# 			count += 1
-# 	return ((r/count), (g/count), (b/count))
-
-line = []
+image_color = []
 
 for color in colors:
 	tail = color + "+color"
-	path = "pictures/" + tail + "/*jpeg" 	
+	path = "pictures/" + tail + "/*jpeg"
 
 	for filename in glob.glob(path):
 		img = Image.open(filename)
-		pix = img.load()
+		# pix = img.load()
 		pix_val = list(img.getdata())
-		rgb_img = img.convert('RGB')
-		r, g, b = rgb_img.getpixel((0, 0))
-		print (r,g,b)
+		# rgb_img = img.convert('RGB')
 		r,g,b = AveragePixelRGB(pix_val)
-		line.append([r,g,b,color])
-
-	csv_name= color + ".csv"
+		image_color.append([r,g,b,color])
+		print(r,g,b, color)
+		
+	csv_name = color + ".csv"
 	with open(csv_name, 'w', newline='') as csvfile:
 		spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		for individual_color in line:
+		for individual_color in image_color:
 			spamwriter.writerow(individual_color)
